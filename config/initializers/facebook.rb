937178e3ec65aeecbd06d9e26c2f88122c5866a9
nzoLogic <nzoLogic@gmail.com>
2017-03-01 11:43:07 -0800

    secrets working

--- config/initializers/facebook.rb
+++ config/initializers/facebook.rb
@@ -1,1 +1,6 @@
-FACEBOOK_CONFIG = YAML.load_file("#{::Rails.root}/config/facebook.yml")[::Rails.env]
+# FACEBOOK_CONFIG = YAML.load_file("#{::Rails.root}/config/facebook.yml")[::Rails.env]
+
+FACEBOOK_CONFIG = {
+  'app_id' => ENV['APP_ID'],
+  'secret' => ENV['SECRET']
+}
